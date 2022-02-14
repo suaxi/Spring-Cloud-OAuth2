cf830c8e6b91bc1b77817f0eaa34cda20664175e
wanghao <281463547@qq.com>
2022-02-14 22:10:30 +0800

    基础环境

---
+++ OAuth-Server/src/main/java/com/sw/oauth/server/OAuthServerApplication.java
@@ -0,0 +1,16 @@
+package com.sw.oauth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Wang Hao
+ * @date 2022/2/14 22:01
+ */
+@SpringBootApplication
+public class OAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthServerApplication.class, args);
+    }
+}
---
+++ OAuth-User/src/main/java/com/sw/oauth/user/OAuthUserApplication.java
@@ -0,0 +1,16 @@
+package com.sw.oauth.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Wang Hao
+ * @date 2022/2/14 22:08
+ */
+@SpringBootApplication
+public class OAuthUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthUserApplication.class, args);
+    }
+}
